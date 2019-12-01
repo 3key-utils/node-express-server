2d46891451f5010a9be286c3fbee4c93c50e6b8f
sahithvibudhi <v.sahithkumar@gmail.com>
2019-12-01 13:58:42 +0530

    health endpoint test added

--- test/server.js
+++ test/server.js
@@ -1,15 +1,26 @@
-const Server = require('../src/');
+const request = require('request');
 const expect = require('chai').expect;
+
+const Server = require('../src/');
+
 const server = new Server();
+const PORT = 5000;
 
 describe('Server tests', function() {
 
-  context('Check for health checks', function() {
-    it('shoyld be able start with no issues    ght', function() {
-      server.serve(5000, () => {
-          console.log(`server started`);
-      });
+  context('Server', function() {
+
+    it('should be able start with no issues', () => {
+      server.serve(PORT, () => {});
     });
+
+    it('check if health endpoint is working', () => {
+        request.get(`http://127.0.0.1:${PORT}/health`, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal('OK');
+        });
+    });
+
   });
 
 });
